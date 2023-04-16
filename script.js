00c06cbb66d333e65f50a474fbe985d5f7e8869e
BenjaminJedar <benjisaintpaul@hotmail.fr>
2023-04-16 20:33:10 +0200

    diverses modifications du style de la page principale + creation d'une page à propos

--- script.js
+++ script.js
@@ -1,7 +1,21 @@
-function afficherMessage() {
+var numSection=1;//numéro de la section
+var section=[document.getElementById("section1"),document.getElementById("section2"),document.getElementById("section3"),document.getElementById("section4"),document.getElementById("section5")];
 
-    let presentation = document.querySelector("#presentation");
-    let message = document.createElement("p");
-    message.innerText = "Salut mon gars !";
-    presentation.appendChild(message); 
-}
+var dy=[];
+dy[1]=0;
+
+for (var i=2;i<=section.length;i++) dy[i]=section[i-1].offsetTop-section[0].offsetTop;
+
+ window.addEventListener("wheel", mouseWheelHandler);
+ 
+ function mouseWheelHandler(e){
+    e.preventDefault();
+    var delta=e.deltaY;
+
+    var doc= document.documentElement /*Chrome, Firefox, IE and Opera*/|| document.body; //  Safari
+    if (delta>0) numSection++; else  numSection--;
+    if (numSection<1) numSection=1;
+    if (numSection>section.length) numSection=section.length;
+    
+    for(var j=1;j<=section.length;j++) if (numSection==j) doc.scrollTop=dy[j];   
+ }
